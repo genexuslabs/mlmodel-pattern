912d5681cbec83279300f487838951fb50835261
Marcos Crispino <mcrispino@genexus.com>
2019-10-04 09:19:19 -0300

    Install GeneXusAI module when applying the pattern (if not installed)

--- GeneXus.Patterns.MLModel/GeneXus.Patterns.MLModel/MLModelBuildProcess.cs
+++ GeneXus.Patterns.MLModel/GeneXus.Patterns.MLModel/MLModelBuildProcess.cs
@@ -1,8 +1,12 @@
-﻿using Artech.Architecture.Common.Objects;
+﻿using Artech.Architecture.BL.Framework.Services;
+using Artech.Architecture.Common.Objects;
+using Artech.Architecture.Common.Services;
+using Artech.Common.Collections;
 using Artech.Genexus.Common;
 using Artech.Genexus.Common.CustomTypes;
 using Artech.Genexus.Common.Objects;
 using Artech.Packages.Patterns.Custom;
+using Artech.Packages.Patterns.Definition;
 using Artech.Packages.Patterns.Engine;
 using Artech.Packages.Patterns.Objects;
 using Artech.Udm.Framework;
@@ -16,6 +20,34 @@ namespace Genexus.Patterns.MLModel
 {
 	internal class MLModelBuildProcess : PatternBuildProcess
 	{
+		/*
+		public override void AfterImportResources(PatternInstance instance)
+		{
+			base.AfterImportResources(instance);
+
+			BLServices.ModuleManager.InstallBuiltIn(instance.Model, "GeneXusAI");
+		}
+		*/
+		public override void BeforeGenerateObjects(PatternInstance instance, IBaseCollection<PatternObject> buildObjects)
+		{
+			base.BeforeGenerateObjects(instance, buildObjects);
+
+			System.Diagnostics.Debug.Assert(false, "MCrispino");
+
+			ModulePackage gxAIModule = null;
+			foreach (IModuleManagerServer server in BLServices.ModuleManager.ListServers())
+			{
+				gxAIModule = server.List().Where(m => m.Name == "GeneXusAI").FirstOrDefault();
+				if (gxAIModule != null)
+					break;
+			}
+
+			if (gxAIModule != null && BLServices.ModuleManager.GetInstalledVersion(instance.Model, gxAIModule) == null)
+			{
+				BLServices.ModuleManager.Install(instance.Model, gxAIModule);
+			}
+		}
+
 		public override void BeforeSaveObjects(PatternInstance instance, InstanceObjects instanceObjects)
 		{
 			base.BeforeSaveObjects(instance, instanceObjects);
